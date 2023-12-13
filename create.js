e255774ab8b6086eac8a43feb52d4746f89f325b
Ozan0062 <ozan0062@edu.zealand.dk>
2023-12-13 11:54:23 +0100

    Create Alerts

--- create.js
+++ create.js
@@ -1,29 +1,38 @@
-const baseUrl = "https://vendingmachinepihat.azurewebsites.net/api/users"
+const baseUrl = "https://vendingmachinepihat.azurewebsites.net/api/users";
 
 Vue.createApp({
     data() {
         return {
-            firstName: null,
-            lastName: null,
-            email: null,
-            mobileNumber: null,
-            id: null,
-            password: null,
-            addData: { firstName: "", lastName: "", email: "", mobileNumber: ""}
+            addData: { firstName: "", lastName: "", email: "", mobileNumber: "" }
         }
-    },
-    async created() {
     },
     methods: {
         async addUser() {
             try {
-                response = await axios.post(baseUrl, this.addData)
+                const response = await axios.post(baseUrl, this.addData, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                // Handle specific error scenarios based on API response
+                if (response.status === 201) {
+                    alert("Bruger oprettet, du vil få en sms med din adgangskode");
+                } else if (response.status === 400) {
+                    // Check if the response data contains error messages
+                    if (response.data.includes("Phone number already exists")) {
+                        alert("Telefonnummer eksisterer allerede");
+                    } else if (response.data.includes("Email already exists")) {
+                        alert("Email eksisterer allerede");
+                    } else {
+                        alert("Ukendt fejl: " + response.data);
+                    }
+                } else {
+                    alert("Ukendt fejl: " + response.data);
+                }
             } catch (ex) {
-                alert(ex.message)
+                alert("Der er en fejl, prøv igen");
             }
         },
     }
-    
-}).mount("#app") 
-
-
+}).mount("#app");
